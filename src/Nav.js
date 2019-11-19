d6e3a08ff1fbf5993566365b7d0a8dd26c95b2f6
bartek <gekarez@gmail.com>
2019-11-19 17:39:48 +0100

    Add example component Nav

---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import logo from './logo.svg';
+
+function Nav() {
+    return (
+        <div>
+            <img src={logo} alt="Error displaying logo image"/>
+        </div>
+    )
+}
+
+export default Nav;
