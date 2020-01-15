47236118e2df16255a09df11fe0c63d541003c00
bartek <gekarez@gmail.com>
2020-01-15 22:14:46 +0100

    Add display product price and product image from backend from static content

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,7 +4,7 @@ class Products extends Component {
     constructor(props, context) {
         super(props, context);
         this.state = {
-            products: [],
+            products: [{id: 0, name: '', price: 0, description: ''}],
         }
     }
 
@@ -21,15 +21,17 @@ class Products extends Component {
     render() {
         return (
             <div className="container">
+                <div className="h1">Products</div>
                 <div className="col-sm-12 text-center">
                     {this.state.products.map(product => {
+                            const img = "http://localhost:8080/images/" + product.id + ".jpg";
                             return (
                                 <div className="row">
                                     <div className="col-sm-2">
-                                        Image
+                                        <img src={img} className="img-size" alt=""/>
                                     </div>
                                     <div className="col-sm-10">
-                                        <h2>{product.name}</h2>
+                                        <h2 className="text-sm-center ">{product.name} | {product.price} PLN</h2>
                                         <p>{product.description}</p>
                                     </div>
                                 </div>);
